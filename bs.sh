6c75e559e3b93c28f7705d564ca88c89c25f3d78
“JaffiSuman” <67546774+JaffiSuman@users.noreply.github.com>
2022-01-10 16:41:51 +0700

    1

--- bs.sh
+++ bs.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
-cp F:/forStudy/GitHub/exper/1.json F:/forStudy/GitHub/JaffiSuman.github.io/1.json
+function fgit {
 git add 1.json 
 git commit  -m "1"
 git push origin main
+sleep 10
+}
+
+for (( i=1; i <= 10; i++ ))
+do
+cp F:/forStudy/GitHub/exper/1.json F:/forStudy/GitHub/JaffiSuman.github.io/1.json
+fgit
+cp F:/forStudy/GitHub/exper/2.json F:/forStudy/GitHub/JaffiSuman.github.io/1.json
+fgit
+cp F:/forStudy/GitHub/exper/3.json F:/forStudy/GitHub/JaffiSuman.github.io/1.json
+fgit
+cp F:/forStudy/GitHub/exper/4.json F:/forStudy/GitHub/JaffiSuman.github.io/1.json
+fgit
+done
